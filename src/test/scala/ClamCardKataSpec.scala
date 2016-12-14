1fb41112bc4e6a4ebc8bd4e67fd52843daf5c45e
Alberto Rodriguez <arodriguez@stratio.com>
2016-12-14 08:34:58 +0100

    ChargerSpec tests for travel within zone A and between zones passing

--- src/test/scala/ClamCardKataSpec.scala
+++ src/test/scala/ClamCardKataSpec.scala
@@ -8,36 +8,35 @@ class ClamCardKataSpec extends FeatureSpec with GivenWhenThen with Matchers {
     info("I want to use my contactless travel card")
     info("So that I can travel around the underground and be charged accordingly")
 
-    /*scenario("Michael travels within ZoneA") {
+    scenario("Michael travels within ZoneA") {
       Given("Michael has a ClamCard")
-      val michaelClamCard = new ClamCard("Michael", 1)
+      val michaelClamCard = new ClamCard("Michael")
       When("He travels from Asterisk to Aldgate")
-      michaelClamCard.travels(Journey("Asterisk", "Aldgate"))
+      Charger.topIn(michaelClamCard, "Asterisk")
+      Charger.topOut(michaelClamCard, "Aldgate")
       Then("He will be charged £2.50 for his journey")
-      val charges = Charger.chargeCard(michaelClamCard)
-      val firstCharge = charges(0)
-      firstCharge.price should be(2.5)
+      val firstCharge = michaelClamCard.journeys(0)
+      firstCharge.charge should be(2.5)
     }
 
     scenario("Michael travels from ZoneA to ZoneB and within ZoneB") {
       Given("Michael has a ClamCard")
-      val michaelClamCard = new ClamCard("Michael", 1)
+      val michaelClamCard = new ClamCard("Michael")
       When("He travels from Asterisk to Barbican")
-      michaelClamCard.travels(Journey("Asterisk", "Barbican"))
+      Charger.topIn(michaelClamCard, "Asterisk")
+      Charger.topOut(michaelClamCard, "Barbican")
       Then("He will be charged £3.00 for his first journey")
-      val charges = Charger.chargeCard(michaelClamCard)
-      val firstCharge = charges(0)
-      firstCharge.price should be(3.0)
+      val firstCharge = michaelClamCard.journeys(0)
+      firstCharge.charge should be(3.0)
       And("He travels from Barbican to Balham")
-      michaelClamCard.travels(Journey("Barbican", "Balham"))
+      Charger.topIn(michaelClamCard, "Barbican")
+      Charger.topOut(michaelClamCard, "Balham")
       And("a further £3.00 for his second journey.")
-      val charge2 = Charger.chargeCard(michaelClamCard)
-
-      charge2 should be(3.0)
-
+      val secondCharge = michaelClamCard.journeys(0)
+      secondCharge.charge should be(3.0)
     }
 
-    scenario("Michael travels from ZoneA to ZoneB, within ZoneB and within ZoneB again") {
+    /*scenario("Michael travels from ZoneA to ZoneB, within ZoneB and within ZoneB again") {
       Given("Michael has a ClamCard")
       val michaelClamCard = new ClamCard("Michael", 1)
       When("He travels from Asterisk to Barbican")
